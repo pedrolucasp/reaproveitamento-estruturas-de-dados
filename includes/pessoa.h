6fdd8c6d41958bd0166f78e5b24b6a73f7debbc8
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2024-09-19 02:41:42 -0300

    includes: add new pet and people structs

---
+++ includes/pessoa.h
@@ -0,0 +1,9 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+struct pessoa {
+    int id;
+    char nome[50];
+};
+
+#endif
---
+++ includes/pet.h
@@ -0,0 +1,10 @@
+#ifndef PET_H
+#define PET_H
+
+struct pet {
+	int id;
+	char nome[50];
+	int urgencia;
+};
+
+#endif
