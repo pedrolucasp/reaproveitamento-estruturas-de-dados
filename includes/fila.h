86c70deff48e9842d3adf1321b5c9423d6281578
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2024-09-19 02:42:49 -0300

    all: modify queue to use new people structure

--- includes/fila.h
+++ includes/fila.h
@@ -1,21 +1,27 @@
 #ifndef FILA_H
 #define FILA_H
 
-struct nodo_fila {
-    int valor;
+#include "pessoa.h"
 
-    struct nodo_fila* prox;
+struct nodo_fila {
+    struct pessoa dado;
+
+    struct nodo_fila *prox;
+    struct nodo_fila *ant;
 };
 
 struct fila {
-    struct nodo_fila* frente;
-    struct nodo_fila* atras;
+    struct nodo_fila *frente;
+    struct nodo_fila *atras;
+
+    int tamanho;
 };
 
 void inicializa_fila(struct fila *f);
-int enfileira(struct fila *f, int valor);
-int desenfileira(struct fila *f, int *valor);
+int enfileira(struct fila *f, struct pessoa p);
+int desenfileira(struct fila *f, struct pessoa *p);
 int fila_vazia(struct fila *f);
+void exibir_fila(struct fila *f);
 void limpa_fila(struct fila *f);
 
 #endif
--- src/fila.c
+++ src/fila.c
@@ -1,68 +1,82 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "fila.h"
+#include "pessoa.h"
 
 void inicializa_fila(struct fila* f) {
 	f->frente = NULL;
 	f->atras = NULL;
 }
 
-int enfileira(struct fila *f, int valor) {
+int enfileira(struct fila *f, struct pessoa p) {
 	struct nodo_fila *item = (struct nodo_fila*) malloc(sizeof(struct nodo_fila));
 
 	if (!item) {
 		return -1;
 	}
 
-	item->valor = valor;
+	item->dado = p;
 	item->prox = NULL;
+	item->ant = f->atras;
 
-	if (f->atras != NULL) {
+	if (f->atras == NULL) {
+		f->frente = item;
+	} else {
 		f->atras->prox = item;
+
 	}
 
 	f->atras = item;
-
-	if (f->frente == NULL) {
-		f->frente = item;
-	}
+	f->tamanho++;
 
 	return 0;
 }
 
-int desenfileira(struct fila *f, int *valor) {
-	struct nodo_fila *temp;
+int desenfileira(struct fila *f, struct pessoa *p) {
+	struct nodo_fila *nodo_removido;
 
 	if (f->frente == NULL) {
 		return -1;
 	}
 
-	temp = f->frente;
-	*valor = temp->valor;
+	nodo_removido= f->frente;
+	*p = nodo_removido->dado;
+
 	f->frente = f->frente->prox;
 
 	if (f->frente == NULL) {
 		f->atras = NULL;
 	}
 
-	free(temp);
+	free(nodo_removido);
+	f->tamanho--;
 
 	return 0;
 }
 
 int fila_vazia(struct fila *f) {
-	return f->frente == NULL;
+	return f->tamanho == 0;
+}
+
+void exibir_fila(struct fila *f) {
+	struct nodo_fila *atual = f->frente;
+	printf("Fila de interessados:\n");
+	if (f->tamanho == 0) {
+		printf("Nenhum interessado na fila.\n");
+		return;
+	}
+
+	while (atual != NULL) {
+		printf("ID: %d, Nome: %s\n", atual->dado.id, atual->dado.nome);
+		atual = atual->prox;
+	}
 }
 
 void limpa_fila(struct fila *f) {
-	struct nodo_fila *atual = f->frente;
+	struct pessoa p;
 
-	while (atual != NULL) {
-		struct nodo_fila *temp = atual;
-		atual = atual->prox;
-
-		free(temp);
+	while (!fila_vazia(f)) {
+		desenfileira(f, &p);
 	}
 
-	f->frente = NULL;
-	f->atras = NULL;
 }
